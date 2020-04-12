4b6273752ba30a0ef8bdcfd127806eb6521ab8e5
Anton <raychek@gmail.com>
2020-04-12 21:21:39 +0300

    initial commit

---
+++ src/main/java/com/sulakov/dbfeeder/ConnectionProvider.java
@@ -0,0 +1,24 @@
+package com.sulakov.dbfeeder;
+
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionProvider {
+    private static final Logger logger = Logger.getLogger(ConnectionProvider.class);
+    private String dbUrl;
+    private String dbUser;
+    private String dbPassword;
+
+    public ConnectionProvider() {
+        dbUrl = PropertiesManager.getProperty("db.url");
+        dbUser = PropertiesManager.getProperty("db.user");
+        dbPassword = PropertiesManager.getProperty("db.password");
+    }
+
+    public Connection getDbConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    }
+}
---
+++ src/main/java/com/sulakov/dbfeeder/CovidCountryStat.java
@@ -0,0 +1,9 @@
+package com.sulakov.dbfeeder;
+
+import org.apache.log4j.Logger;
+
+public class CovidCountryStat {
+    private static final Logger logger = Logger.getLogger(CovidCountryStat.class);
+
+
+}
